c5a51e93641b27107fdf01adeddb6192cbb91a64
Kübra Kutlu <kubra.kutlu@aalto.fi>
2020-01-15 18:00:02 +0300

    implemented equilibrium function in c++

--- equi/cpp/EquiTest.cpp
+++ equi/cpp/EquiTest.cpp
@@ -3,7 +3,32 @@
 int equi(int inputArr[], int arrayLen)
 {
     // Find equilibrium index of inputArr
-    return 0;
+    int sumAll = 0; // Total sum of the array
+    int sumLeft = 0; // Sum of the elements on the left hand side of equilibrium index
+
+    if (arrayLen==0) //Empty array
+        return -1;
+    else
+    {
+        // Calculate total sum of the array
+        for (int i = 0; i < arrayLen; i++)
+            sumAll += inputArr[i];
+
+        /* Equilibrium state is found by subtracting left sum from total sum, then
+        comparing this remainder sum to left sum. */
+        for (int i = 0; i < arrayLen; i++)
+        {
+            sumAll -= inputArr[i]; // Remainder sum is equal to right sum for index i
+            // Check equilibrium state.
+            if (sumLeft == sumAll)
+                return i;
+
+            sumLeft += inputArr[i]; // Update left sum
+        }
+
+        // Return -1 in case of no equilibrium index found
+        return -1;
+    }
 }
 
 TEST(EquiTest, EmptyArrayReturnsZero)
